0eb9ab3c8d2a285f9c2771516f586f013fbe8a55
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2019-05-02 22:08:43 +0100

    aggregated version color coding

--- public/javascripts/js/import-export.js
+++ public/javascripts/js/import-export.js
@@ -8,7 +8,12 @@
   window.toggleMarking = toggleMarking;
   window.isMarkingsOn = true;
   window.currentText = undefined;
-  
+  const sum = (accumulator, currentValue) => accumulator + currentValue;
+  var aggregatedTotals = {
+    totalWordMatch: [],
+    someotherTotal: []
+
+  }; 
 
   function loadCSV(input) {
     var reader = new FileReader();
@@ -33,30 +38,48 @@
     });
   }
   function loadCurrentTextOnToDom() {
+    
     // var inputRows = d3.csvParseRows(inputText);
     var dataTable = document.getElementById('dataTable');
     var tableBody = dataTable.querySelector('tbody');
     var inputRows = d3.tsvParseRows(window.currentText);
     console.log(inputRows);
-    console.log(inputRows[2][8].length);
-
+    //console.log(inputRows[2][8].length);
     
-    var col = {
-      name1: 0,
-      name2: 10,
-      content1: 8,
-      content2: 18
-      
-    };
+
+    if (localStorage.getItem('Isaggregated') == 2) {
+
+      var col = {
+        name1: 0,
+        name2: 10,
+        content1: 8,
+        content2: 18
+
+      };
+    } else {
+
+      var col = {
+        name1: 10,
+        name2: 11,
+        content1: 15,
+        content2: 16,
+        b1: 0
+
+      };
+
+    }
+
     d3.selectAll('#dataTable tr:not(#rowTemplate)').remove();
     d3.select('.row-count').selectAll('p').remove();
     d3.select('.row-count').append('p')
     .text("Number of records (aligned pairs): " + inputRows.length);
-  
+    
+
     processRow(inputRows.shift(), false);    // ##1 line deals first row as header
 
     inputRows.forEach(function (dataRow) {
       processRow(dataRow, window.isMarkingsOn);
+      aggregatedTotals.totalWordMatch.push(parseInt(dataRow[col.b1]));
     });
 
     d3.select('#dataTable').style('display', null);
@@ -64,10 +87,11 @@
     function processRow(dataRow, isMarkingOn) {
       var nodeClone = document.getElementById('rowTemplate').cloneNode(true);
       nodeClone.removeAttribute('id');
+     
       var params = {
         book1Name: dataRow[col.name1],
         book2Name: dataRow[col.name2]
- 
+
 
       };
       if (isMarkingOn) {
@@ -76,22 +100,25 @@
       } else {
         params.book1Content = dataRow[col.content1];
         params.book2Content = dataRow[col.content2];
-        
+
       }
       nodeClone.querySelectorAll('td').forEach(function (td) {
         td.innerHTML = replaceParams(td.innerHTML, params);
-        
+
       });
       nodeClone.removeAttribute('hidden');
       tableBody.append(nodeClone);
     }
+    //might be useful. showing aggregated totals...
+   // console.log(aggregatedTotals.totalWordMatch.reduce(sum))
   }
+  
   function exportCSV() {
     if (document.getElementById('fileInput').files.length === 0) {
       return;
     }
     var csvOutputArray = [];
-    
+
     d3.selectAll('#dataTable tr:not(#rowTemplate)')
       .each(function () {
         var outputRow = [];
